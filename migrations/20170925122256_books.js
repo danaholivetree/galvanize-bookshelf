b8ee244fa0443c44d3173e435db8b941ec4c0c82
danah olivetree <danah.olivetree@gmail.com>
2017-09-25 14:15:23 -0600

    migration works

--- migrations/20170925122256_books.js
+++ migrations/20170925122256_books.js
@@ -1,18 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('books', function(table) {
+  return knex.schema.createTableIfNotExists('books', function(table) {
     table.increments()
-    table.varchar('title', 255).notNullable()
-    table.varchar('author', 255).notNullable()
-    table.varchar('genre', 255).notNullable()
-    table.text('description').notNullable()
-    table.text('cover_url').notNullable()
-    table.timestamp('created_at').notNullable()
-    table.timestamp('updated_at').notNullable()
-    )
+    table.varchar('title', 255).notNullable().defaultTo("")
+    table.varchar('author', 255).notNullable().defaultTo("")
+    table.varchar('genre', 255).notNullable().defaultTo("")
+    table.text('description').notNullable().defaultTo("")
+    table.text('cover_url').notNullable().defaultTo("")
+    table.timestamps(true, true)
   })
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTableIfExists('books')
 };
