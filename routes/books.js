61a7a7f3ccc954677f2323d1b39c5ce7e7925901
danah olivetree <danah.olivetree@gmail.com>
2017-09-26 15:41:57 -0600

    some new stuff works

--- routes/books.js
+++ routes/books.js
@@ -7,74 +7,99 @@ const router = express.Router();
 const knex = require('../knex')
 const camel = require('to-camel-case')
 
-router.get('/books', (req, res, next) => {
+router.get('/', (req, res, next) => {
   knex('books')
     .select()
     .orderBy('title')
-    .then((items) => {
+    .then( (items) => {
       res.setHeader('Content-Type', 'application/json')
       res.status(200)
-      res.send(camel(JSON.stringify(items)))
+      res.send(JSON.stringify(items))
     })
 })
 
-router.get('/books/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   const id = req.params.id
   knex('books')
     .select()
     .where('id', id)
+    .orderBy('id')
     .then((items) => {
       if (items.length < 1) {
         return res.sendStatus(404)
       }
       res.setHeader('Content-Type', 'application/json')
       res.status(200)
-      res.send(camel(JSON.stringify(items[0])))
+      //res.send(JSON.stringify(items[0]))
+      res.json(items[0])
     })
 })
 
-router.post('/books', (req, res, next) => {
+router.post('/', (req, res, next) => {
   const item = req.body
   knex('books')
-    .insert(item, 'id')
+    .insert(item)
+    .then( () => {
+    //   .select()
+    //   .where('id', knex.raw( (" SELECT MAX(id) FROM 'books' ") ;))
+    // })
+    // "SELECT setval('books_id_seq', (SELECT MAX(id) FROM books))"
+
     .then( () => {
       res.status(200)
+      res.setHeader('Content-Type', 'application/json')
       //res.send(JSON.stringify(item))
-      res.json(item)
+      res.json()
     })
 })
 
-router.patch('/books/:id', (req, res, next) => {
-  console.log("this is the request body for a patch: ", req.body)
+router.patch('/:id', (req, res, next) => {
   const id = req.params.id
+  let newBook = {}
   if (req.body.title) {
-
+    let newBook.title = req.body.title
   }
 
+  if (req.body.author) {
+    let newBook.author = req.body.author
+  }
+  if (req.body.genre) {
+    let newBook.genre = req.body.genre
+  }
+  if (req.body.description) {
+    let newBook.description = req.body.description
+  }
+  if (req.body.cover_url) {
+    let newBook.cover_url = req.body.cover_url
+}
   knex('books')
-  .update({'title':req.body.title}, {'author':req.body.author}, {'genre': req.body.genre}, {'description': req.body.description}, {'cover_url': req.body.cover_url}, 'id')
-  .where('id', id)
-  .then((id) => {
-    // if (rowsAffected !== 1) {
-    //   return res.sendStatus(404)
-    // }
-    res.status(200)
-    res.setHeader('Content-Type', 'application/json')
-    res.send(rowsAffected)
-  })
-  .catch((err) => next(err))
+    .select()
+    .where('id', id)
+    .then( (item) => {
+      if (item.length < 1) {
+        res.sendStatus(404)
+      }
+    else {
+      .update(newBook, *)
+      .where('id', id)
+      .then( (stuff) => {
+        res.status(200)
+        res.setHeader('Content-Type', 'application/json')
+        res.send(stuff)
+      })
+    .catch( (err) => next(err) )
+    }
 })
 
 router.delete('/:id', function (req, res, next) {
-  console.log(req.body);
   const id = req.params.id
 
   knex('books')
     .del()
     .where('id', id)
-    .then((rowsAffected) => {
-      if (!rowsAffected) {
-        return res.sendStatus(404)
+    .then((stuff) => {
+      if (!stuff) {
+        throw boom.create(404, 'Not Found')
       }
       res.sendStatus(200)
       res.setHeader('Content-Type', 'application/json')
--- seeds/999_sequences.js
+++ seeds/999_sequences.js
@@ -1,8 +1,5 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
+
   return knex.raw("SELECT setval('books_id_seq', (SELECT MAX(id) FROM books))")
 };
-
-
-//     return knex.raw(SELECT setval('knex_migrations_id_seq', (SELECT MAX(id) FROM books)))
