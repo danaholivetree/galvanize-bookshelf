1e84076e21188ab091457d98671cebb4eea519cc
danah olivetree <danah.olivetree@gmail.com>
2017-09-29 12:25:41 -0600

    passed migrations 5

--- migrations/20170929120337_favorites.js
+++ migrations/20170929120337_favorites.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('favorites', (t) => {
     t.increments()
-    t.integer('book_id').notNullable().references("books.id").onDelete('cascade')
-    t.integer('user_id').notNullable().references("users.id").onDelete('cascade')
+    t.integer('user_id').notNullable().references("users.id").onDelete('cascade').index()
+    t.integer('book_id').notNullable().references("books.id").onDelete('cascade').index()
     t.timestamps(true, true)
   })
 };
